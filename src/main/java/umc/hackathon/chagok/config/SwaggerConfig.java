50cf213edf3c2db29f0e1b21d038fe4cf8fbea49
yerim <yerimm99@github.com>
2024-01-02 20:44:11 +0900

    [Feat] SwaggerConfig 세팅

--- src/main/java/umc/hackathon/chagok/config/SwaggerConfig.java
+++ src/main/java/umc/hackathon/chagok/config/SwaggerConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public GroupedOpenApi chagokOpenApi() {
         String[] paths = {"/v1/**"};
