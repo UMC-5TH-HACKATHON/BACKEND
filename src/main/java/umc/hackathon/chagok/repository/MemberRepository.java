e490b563ccfdfa051df1014c5b200d42daee98eb
koreanair777 <jinuklee777@soongsil.ac.kr>
2024-01-02 23:24:24 +0900

    :sparkles: [FEAT] 특정 회원의 박스 조회 API

--- src/main/java/umc/hackathon/chagok/repository/MemberRepository.java
+++ src/main/java/umc/hackathon/chagok/repository/MemberRepository.java
@@ -1,7 +1,13 @@
 package umc.hackathon.chagok.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.hackathon.chagok.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Override
+    Optional<Member> findById(Long aLong);
+
+    Optional<Member> findByEmailAndPassword(String email, String password);
 }
--- src/main/java/umc/hackathon/chagok/service/MemberService/MemberService.java
+++ src/main/java/umc/hackathon/chagok/service/MemberService/MemberService.java
@@ -1,4 +1,9 @@
 package umc.hackathon.chagok.service.MemberService;
 
+import java.util.List;
+import umc.hackathon.chagok.web.dto.MemberRequest;
+
 public interface MemberService {
+    public List<Boolean> boxCheck(Long memberId, Integer mm, Integer yy);
+
 }
